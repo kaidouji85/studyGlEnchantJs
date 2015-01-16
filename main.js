b06397de4e372ec2b7f9daada3f498bd21df5991
kaidouji85 <kaidouji85@gmail.com>
2015-01-17 01:46:18 +0900

    Cubeに対してTimelineをつけることができた。

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ window.onload = function() {
         scene = new Scene3D(); //3Dのシーンを作る
         obj = new Cube(); //立方体を作る
         scene.addChild(obj); //3Dシーンに立方体を追加
+
+        obj.tl = new enchant.Timeline(obj);
+        obj.tl.delay(30).then(function(){
+            console.log('コールバック実行');
+        });
     };
     game.start();
 };
